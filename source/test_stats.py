c83571d6892af0c8819c984f7e2db13f67c72409
Sersan Guedes <sersan.guedes@RIOENERGY.LOCAL>
2020-09-24 15:24:06 -0300

    meu segundo commit

--- source/test_stats.py
+++ source/test_stats.py
@@ -1,6 +1,6 @@
 from  aux_lib import stats as st
 from sklearn.linear_model import LinearRegression
-#from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
 
